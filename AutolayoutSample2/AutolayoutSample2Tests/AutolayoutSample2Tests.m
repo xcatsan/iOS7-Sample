0c52c42781187191dd807a08a16195abf34cb4ad
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-01-07 08:49:57 +0900

    Added AutolayoutSample2

---
+++ AutolayoutSample2/AutolayoutSample2Tests/AutolayoutSample2Tests.m
@@ -0,0 +1,34 @@
+//
+//  AutolayoutSample2Tests.m
+//  AutolayoutSample2Tests
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/07.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutolayoutSample2Tests : XCTestCase
+
+@end
+
+@implementation AutolayoutSample2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
